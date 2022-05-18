2291c110bcfc15ccd52d84c2905117660c4e27fc
MaybeIamCandy <91553215+MaybeIamCandy@users.noreply.github.com>
2022-05-18 17:21:12 +0800

    求水仙花数

---
+++ sy10-1.py
@@ -0,0 +1,23 @@
+'''
+Sy10-1 求出所有的水仙花数，保存到Arms.txt文件。
+'''
+
+def armstrong():
+    addupsum = 0
+    open('Arms.txt', 'w', encoding='utf-8')
+    for i in range(100,1000):
+        num = str(i)
+        for j in range(len(num)):
+            addupsum += int(num[j])**3
+        if addupsum == int(num):
+            addupsum = 0
+            #print(num)
+            f = open('Arms.txt', 'a', encoding='utf-8')
+            f.write('{}\n'.format(num))
+        else:
+            addupsum = 0
+            
+    print('End')
+    f.close()
+
+armstrong()
